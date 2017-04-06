7f9311000a1cbca804886d3313b676593f0de014
Alex España <A01422673@itesm.mx>
2017-04-06 10:44:26 -0500

    Agregué Bootstramp al repo
    None

---
+++ Login.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>LOG IN DELTA</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">   
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
+    <link rel="stylesheet" href="clases.css">
+    <div class="bs-docs-header"></div>
+</head>
+<!--<img src="C:\Users\Pronficilio\Pictures\Alex\MolesterMoon.png " height=50 width=50 alt="..." class="img-circle">
+-->
+<body>
+    
+    <form class="form-horizontal loginbox">
+            <div class="form-group">
+                <label for="inputEmail3" class="col-sm-5 control-label"></label>
+                <div class="col-sm-3">
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                </div>
+        </div>
+        <div class="form-group">
+                <label for="inputPassword3" class="col-sm-5 control-label"></label>
+                <div class="col-sm-3">
+                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                </div>
+        </div>
+        <div class="form-group">
+                <div class="col-sm-offset-6 col-sm-10">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox"> Remember me
+                        </label>
+                    </div>
+                </div>
+        </div>
+        <div class="form-group">
+                <div class="col-sm-offset-6 col-sm-10">
+                    <button type="button" class="btn btn-primarymy btn-lg active">Sign in</button>
+                </div>
+        </div>
+    
+    </form>
+<script src="js/jquery.js"> </script>
+<script src="js/bootstrap.min.js"> </script>
+
+</body>
+</html>
